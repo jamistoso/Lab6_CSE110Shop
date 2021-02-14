6b1d8f05472680e2f175c75966b943c9fcb0c74c
jamistoso <jamistos@ucsd.edu>
2021-02-13 23:49:35 -0800

    did the lab

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,57 @@
 // product-item.js
 
 class ProductItem extends HTMLElement {
-  // TODO
+  constructor(url, urlAlt, p1Text, p2Text, num) {
+    super();
+
+    const number = num.toString();
+
+    let shadow = this.attachShadow({mode: 'open'});
+    const wrapper = document.createElement('li');
+    wrapper.setAttribute('class', "product");
+
+    const picture = document.createElement('img');
+    picture.src = new URL(url.substring(1,url.length-1)).href;
+    picture.setAttribute('alt', urlAlt);
+    picture.setAttribute('max-height', "100%");
+    
+    const p1 = document.createElement('p');
+    p1.setAttribute('class', "title");
+    p1.textContent = p1Text.substring(1,urlAlt.length-1);
+
+    const p2 = document.createElement('p');
+    p2.setAttribute('class', "price");
+    const p2dollar = '$';
+    p2.textContent = p2dollar.concat(p2Text);
+
+    let butStr = 'button';
+    butStr = butStr.concat(num.toString());
+    const button = document.createElement('button');
+    button.setAttribute('class', 'checkout');
+    button.setAttribute("tagName", 'checkout');
+    if(localStorage.getItem(butStr) == "false" || localStorage.getItem(butStr) == null) {
+      button.textContent = "Add to Cart";
+    } else {
+      button.textContent = "Remove from Cart";
+    }
+    button.setAttribute('id', butStr);
+    button.addEventListener("click", function() {
+      {handleClick (button)}
+    });
+
+    // Apply external styles to the shadow dom
+    const linkElem = document.createElement('link');
+    linkElem.setAttribute('rel', 'stylesheet');
+    linkElem.setAttribute('href', 'styles/styles.css');
+
+    // Attach the created element to the shadow dom
+    shadow.appendChild(linkElem);
+    shadow.appendChild(wrapper);
+    wrapper.appendChild(picture);
+    wrapper.appendChild(p1);
+    wrapper.appendChild(p2);
+    wrapper.appendChild(button);
+  }
 }
 
 customElements.define('product-item', ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,63 @@
 // Script.js
+var arrLength;
+var cartNum;
+var wrapper;
+
 
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+  if(localStorage.getItem('product0title') == null) {
+    fetch('https://fakestoreapi.com/products')
+    .then(response => response.json())
+    .then(data => {
+      i = 0;
+      arrLength = data.length;
+      for (i = 0; i < data.length; i++) {
+        s1 = 'product';
+        s1 = s1.concat(i.toString());
+        a = JSON.stringify(data[i].title);
+        localStorage.setItem(s1.concat('title'), a);
+        b = JSON.stringify(data[i].price);
+        localStorage.setItem(s1.concat('price'), b);
+        c = JSON.stringify(data[i].image);
+        localStorage.setItem(s1.concat('image'), c);
+        newProduct = new ProductItem(c, a, a, b, i);
+        textContainer = document.getElementById('product-list');
+        textContainer.appendChild(newProduct);
+        
+      }
+      cartNum = 0;
+      localStorage.setItem("cartNum", 0);
+    });
+  } else {
+    i = 0;
+    for (i = 0; i < 20; i++) {
+      s1 = 'product';
+      s1 = s1.concat(i.toString());
+      a = localStorage.getItem(s1.concat('title'));
+      b = localStorage.getItem(s1.concat('price'));
+      c = localStorage.getItem(s1.concat('image'));
+      newProduct = new ProductItem(c, a, a, b, i);
+      textContainer = document.getElementById('product-list');
+      textContainer.appendChild(newProduct);
+      
+    }
+    document.getElementById("cart-count").textContent = localStorage.getItem("cartNum");
+  }
 });
+
+function handleClick(button) {
+  console.log(button.textContent);
+  if(button.textContent == "Add to Cart") {
+    document.getElementById("cart-count").textContent = parseInt(document.getElementById("cart-count").textContent) + 1;
+    button.textContent = "Remove from Cart";
+    s1 = button.id;
+    localStorage.setItem(s1, "true");
+    localStorage.setItem("cartNum", parseInt(document.getElementById("cart-count").textContent));
+  } else {
+    document.getElementById("cart-count").textContent = parseInt(document.getElementById("cart-count").textContent) - 1;
+    button.textContent = "Add to Cart";
+    s1 = button.id;
+    localStorage.setItem(s1, "false");
+    localStorage.setItem("cartNum", parseInt(document.getElementById("cart-count").textContent));
+  }
+}
